bfe22fc25e956eb3c80c35b4757b51221ac90d43
sesa371138 <grzegorz.banda@se.com>
2022-03-29 11:40:35 +0200

    [system] (init) structure setup

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(cb){
+    gulp
+        .src('less/sigil_and_shadow.less')
+        .pipe(less())
+        .pipe(gulp.dest("./styles/"));
+    cb();
+});
+
+gulp.task('sigil_and_shadow', function(cb){
+    gulp.watch('less/*.less', gulp.series('less'));
+});

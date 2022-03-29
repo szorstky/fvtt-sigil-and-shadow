35db8577e3354ae06dd74fe3fce47012306792b3
szorstky <greg.banda@gmail.com>
2022-03-29 21:48:40 +0200

    [actor] (feat) data persistance

--- module/sheets/SigilPlayerCharacterSheet.js
+++ module/sheets/SigilPlayerCharacterSheet.js
@@ -42,13 +42,13 @@ export default class SigilPlayerCharacterSheet extends ActorSheet {
         data.actor = actorData;
         data.data = actorData.data;
 
-        data.oddities = data.items.filter(function(item){ return item.type == "oddity"});
-        data.descriptors = data.items.filter(function(item){ return item.type == "descriptor"});
-        data.perks = data.items.filter(function(item){ return item.type == "perk"});
-        data.powers = data.items.filter(function(item){ return item.type == "power"});
+        data.oddities = actorData.items.filter(function(item){ return item.type == "oddity"});
+        data.descriptors = actorData.items.filter(function(item){ return item.type == "descriptor"});
+        data.perks = actorData.items.filter(function(item){ return item.type == "perk"});
+        data.powers = actorData.items.filter(function(item){ return item.type == "power"});
 
-        data.equipment = data.items.filter(function(item){ return item.type == "equipment"});
-        data.weapons = data.items.filter(function(item){ return item.type == "weapon"});
+        data.equipment = actorData.items.filter(function(item){ return item.type == "equipment"});
+        data.weapons = actorData.items.filter(function(item){ return item.type == "weapon"});
         
         return data;
     }
